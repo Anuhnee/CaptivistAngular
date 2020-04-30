001a97db78656c386b35f352df84eaf36ad23f7f
Anthony Perez <antrp93@gmail.com>
2020-04-29 20:12:37 -0500

    fixed routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule,Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'userprofile', component: UserProfileComponent},
   {path: '**', component: LoginComponent}
 ]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
   ]
 })
 export class AppRoutingModule { }
